ceed1e17ca1aeb35ddfe78f93671e4ba53c09ccb
YuyaShimo <mei300225@gmail.com>
2020-04-08 20:10:59 +0900

    タイムラインクラスを作成

---
+++ Like&HeatSNS/Model/TimeLineModel.swift
@@ -0,0 +1,15 @@
+//
+//  TimeLineModel.swift
+//  Like&HeatSNS
+//
+//  Created by 下新原佑哉 on 2020/04/08.
+//  Copyright © 2020 Yuya shimoshimbara. All rights reserved.
+//
+
+import Foundation
+import  Firebase
+
+class TimeLineModel {
+    
+    
+}
